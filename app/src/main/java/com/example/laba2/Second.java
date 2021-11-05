f631ddbaf17747589df1a7fdd0a2b23a3b9c4430
Koptelova Valentina <kotik.jonas@gmail.com>
2021-11-05 23:07:47 +0300

    Initial commit

---
+++ app/src/main/java/com/example/laba2/Second.java
@@ -0,0 +1,11 @@
+package com.example.laba2;
+
+import android.app.Activity;
+import android.os.Bundle;
+public class Second extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+    }
+}
